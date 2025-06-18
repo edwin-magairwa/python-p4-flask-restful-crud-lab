cf30878240c17848bd877ac966bfe65ab456e5f1
edwin magairwa <edwinmagairwa@gmail.com>
2025-06-18 15:21:45 +0300

    added python-flask

--- server/app.py
+++ server/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db, Plant
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -49,6 +52,34 @@ class PlantByID(Resource):
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = db.session.get(Plant, id)
+    if not plant:
+        abort(404)
+    data = request.get_json()
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+        db.session.commit()
+    return jsonify({
+        "id": plant.id,
+        "name": plant.name,
+        "image": plant.image,
+        "price": plant.price,
+        "is_in_stock": plant.is_in_stock
+    }), 200
+
+
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = db.session.get(Plant, id)
+    if not plant:
+        abort(404)
+    db.session.delete(plant)
+    db.session.commit()
+    return '', 204
 
 
 if __name__ == '__main__':
